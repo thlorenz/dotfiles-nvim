8c589b71aa2e3c998c4cd2791207f87fd85b491a
Thorsten Lorenz <thlorenz@gmx.de>
2022-06-27 10:38:46 -0600

    rust-tools: add rust analyzer schema

--- lua/sx/plugin/rust-tools/init.lua
+++ lua/sx/plugin/rust-tools/init.lua
@@ -49,8 +49,8 @@ M.setup = function()
 			-- on_attach is a callback called when the language server attachs to the buffer
 			-- on_attach = on_attach,
 			settings = {
-				-- to enable rust-analyzer settings visit:
 				-- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+				-- Schema: https://github.com/simrat39/rust-tools.nvim/blob/master/ci/schema/output.md
 				["rust-analyzer"] = {
 					updates = {
 						channel = "nightly",
